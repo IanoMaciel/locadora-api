adfe1b4659d41cacbe18f9ccff1a55fcb61ddd71
Iano Maciel <ianomaciel6385@gmail.com>
2024-02-23 17:22:05 -0400

    feat: validation

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -5,25 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 
-class MarcaController extends Controller
-{
+class MarcaController extends Controller {
+
+    public function __construct (Marca $marca) {
+        $this->marca = $marca;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        // paginação
-        $marcas = Marca::paginate(10);
-
-        // caso não haja nenhum registro
+        $marcas = $this->marca->paginate(10);
         if ($marcas->isEmpty())
             return response()->json(['message' => 'No records found'], 404);
 
         return response()->json($marcas);
-
-        // sem paginação
-        //return Marca::all();
     }
 
     /**
@@ -43,18 +41,23 @@ class MarcaController extends Controller
             );
         }
 
-        return Marca::create($request->all());
+        // return Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
+        return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca) {
-        if(!$marca)
-            return response(['message' => 'Invalid id'], 422);
+    public function show($id) {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response(['message' => 'marca not found'], 404);
+        }
 
         return $marca;
     }
@@ -63,22 +66,32 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca) {
+    public function update(Request $request, $id) {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null)
+            return response(['message' => 'brand not found'], 404);
+
         $marca->update($request->all());
+
         return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
-    {
-        //
+    public function destroy($id) {
+        $marca = $this->marca->find($id);
+
+        if(!$marca) return response()->json(['message' => 'brand not found'], 404);
+
+        $marca->delete();
+        return response()->json(['message' => 'Product deleted'], 200);
     }
 }
