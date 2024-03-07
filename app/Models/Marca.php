7bea0d4853b482f1239018f4d2784536cf090788
Iano Maciel <ianomaciel6385@gmail.com>
2024-03-07 08:25:05 -0400

    feat: criação da classe model

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -12,15 +12,18 @@ class Marca extends Model {
     public function rules() {
         return [
             "nome" => "required|unique:marcas,nome,'.$this->id.'|min:3",
-            "imagem" => "required"
+            "imagem" => "required|file|mimes:png"
         ];
     }
 
     public function feedback() {
         return [
-            "required" => "O campo :attribute é obrigatório",
-            "nome.unique" => "O nome da marca já existe",
-            "nome.min" => "O nome deve ter no mínimo 3 caracteres"
+            "required" => "O campo :attribute é obrigatório.",
+            "nome.unique" => "O nome da marca já existe.",
+            "nome.min" => "O nome da marca deve ter pelo menos 3 caracteres.",
+            "imagem.required" => "O campo :attribute é obrigatório.",
+            "imagem.file" => "O :attribute deve ser um arquivo.",
+            "imagem.mimes" => "O :attribute deve ser um arquivo PNG."
         ];
     }
 }
