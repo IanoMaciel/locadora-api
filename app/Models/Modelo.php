44fa5b0f552a58a62b261c3e2c746b96ac3388a9
Iano Maciel <ianomaciel6385@gmail.com>
2024-03-07 09:55:25 -0400

    feat: desenvolvimento da classe Model da tabela modelos. Além disso, fora adicionadas as regras de validação

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -5,7 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Modelo extends Model
-{
+class Modelo extends Model {
     use HasFactory;
+    protected $fillable = [
+        'marca_id',
+        'nome',
+        'imagem',
+        'numero_portas',
+        'lugares',
+        'air_bag',
+        'abs',
+    ];
+
+    public function rules() {
+        return [
+            'marca_id' => 'required|integer|exists:marcas,id',
+            'nome' => 'required|unique:modelos,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png,jpeg, jpg',
+            'numero_portas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean', //true or false, 1 or 0 and, "1" or "0"
+            'abs' => 'required|boolean',
+        ];
+    }
+
 }
