8df4b51c681f86e37efc34f323aa078138d6b027
Iano Maciel <ianomaciel6385@gmail.com>
2024-03-12 10:44:10 -0400

    refactor: criação de AbstractRefactory

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -2,32 +2,5 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class MarcaRepository {
-    protected $model;
-
-    public function __construct(Model $model) {
-        $this->model = $model;
-    }
-    public function selectAtributosRegistrosRelacionados($atributos) {
-        // guarda o estado que a query esta sendo construída
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro($filtros) {
-        $filtros = explode(';', $filtros);
-        foreach ($filtros as $key => $condicao) {
-            $c = explode(':', $condicao);
-            $this->model = $this->model->where($c[0], $c[1], $c[2]); // a query está sendo construída
-        }
-    }
-
-    public function selectAtributos($atributos) {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado() {
-        return $this->model->paginate(10);
-    }
+class MarcaRepository extends AbstractRepository{
 }
